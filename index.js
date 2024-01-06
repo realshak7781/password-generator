5dfb3c8242114693684c7758e12c99da6d89151d
@fit_Monk <124501803+realshak7781@users.noreply.github.com>
2024-01-06 18:46:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function getRandomString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
+    }
+  
+    return result;
+  }
+  
+ document.getElementsByTagName("button")[0].addEventListener("click",function(){
+    var randomString=getRandomString(10);
+    document.getElementsByTagName("input")[0].value=randomString;
+});
+
+
+
